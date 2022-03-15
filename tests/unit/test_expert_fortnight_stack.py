7836fac830022a6a678c226f4e9d5feb9ce4cc1a
Esakkimuthu Natarajan <esakkimuthu.natarajan@myer.com.au>
2022-03-15 19:33:25 +0530

    Initial commit

---
+++ tests/unit/test_expert_fortnight_stack.py
@@ -0,0 +1,19 @@
+# from aws_cdk import (
+#         core,
+#         assertions
+#     )
+
+# from expert_fortnight.expert_fortnight_stack import ExpertFortnightStack
+
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in expert_fortnight/expert_fortnight_stack.py
+def test_sqs_queue_created():
+#     app = core.App()
+#     stack = ExpertFortnightStack(app, "expert-fortnight")
+#     template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
+  pass
