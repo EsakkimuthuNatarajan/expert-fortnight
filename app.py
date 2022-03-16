112e538535014cbf9444c923aed18f05abccfdb2
Esakkimuthu Natarajan <esakkimuthu.natarajan@myer.com.au>
2022-03-16 12:26:47 +0530

    changing region

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ from aws_cdk import core as cdk
 from custom_resources.custom_ec2 import WebserverStack
 from custom_resources.custom_vpc import TestVpcStack
 # from expert_fortnight.expert_fortnight_stack import ExpertFortnightStack
-# env = cdk.Environment( region= "us-east-1", account= "275239396717")
+# env = cdk.Environment( region= "us-west-2", account= "275239396717")
 
 app = cdk.App()
-vpc_Stack = TestVpcStack(app, "Vpc-Stack", env=cdk.Environment(account='275239396717', region='us-east-1'))
-ec2_Stack = WebserverStack(app, "WebServer-Stack", env=cdk.Environment(account='275239396717', region='us-east-1'))
+vpc_Stack = TestVpcStack(app, "Vpc-Stack", env=cdk.Environment(account='275239396717', region='us-west-2'))
+ec2_Stack = WebserverStack(app, "WebServer-Stack", env=cdk.Environment(account='275239396717', region='us-west-2'))
 
 app.synth()
